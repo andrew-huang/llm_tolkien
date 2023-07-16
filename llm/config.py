9d3c4a0202c885591f49191af33fd5dd5b6914e5
eitan <eitan.turok@gmail.com>
2023-07-16 19:31:09 -0400

    make mosaic-storywriter the model

--- llm/config.py
+++ llm/config.py
@@ -15,9 +15,7 @@ extraction_path = REPO_DIR / "llm/data/extracted_text.jsonl"
 min_length = 100
 
 # HF repo
-# hf_repo = "JeremyArancio/llm-tolkien"
-hf_repo = "eturok/llm-tolkien"
-hf_book_repo = "eturok/lotr-book"
+hf_repo = "eturok/mpt-7b-storywriter-tolkien"
 
 # Dataset
 context_length = 2048
@@ -26,14 +24,14 @@ test_size = 0.1
 shuffle = True
 
 # Training
-model_name = 'bigscience/bloom-3b'
+model_name = 'mosaicml/mpt-7b-storywriter'
 lora_r = 16 # attention heads
 lora_alpha = 32 # alpha scaling
 lora_dropout = 0.05
 lora_bias = "none"
 lora_task_type = "CAUSAL_LM" # set this for CLM or Seq2Seq
 
-## Trainer config
+# Trainer config
 per_device_train_batch_size = 1 
 gradient_accumulation_steps = 1
 warmup_steps = 100 
@@ -45,12 +43,14 @@ logging_steps = 1
 overwrite_output_dir = True
 evaluation_strategy = "no"
 save_strategy = "no"
-push_to_hub = False
+push_to_hub = True
+report_to = 'wandb'
+run_name = 'lotr-llm'
 
-## Data collator
+# Data collator
 mlm =False
 
-## Generate
-max_new_tokens = 50
-temperature = 0.5
+# Generate
+max_new_tokens = 100
+temperature = 0.7
 do_sample = False
